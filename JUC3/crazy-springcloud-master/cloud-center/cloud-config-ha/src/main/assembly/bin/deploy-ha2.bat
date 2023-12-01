4f1fa58ad7051402210745a125eda574e12234f8
Nick Hou <houchj@gmail.com>
2023-12-01 17:45:18 +0800

    config server with ha is successful

--- JUC3/crazy-springcloud-master/cloud-center/cloud-config-ha/src/main/assembly/bin/deploy-ha2.bat
+++ JUC3/crazy-springcloud-master/cloud-center/cloud-config-ha/src/main/assembly/bin/deploy-ha2.bat
@@ -1,12 +1,14 @@
 echo off
 title ConfigServerApplication
-set SERVER_PORT=7788
+set SERVER_PORT=7789
+set EUREKA_PORT=7778
+set EUREKA_HOST=eureka-ha2
 set JAR_NAME=config-ha-1.0-SNAPSHOT.jar
 set MAIN_CLASS=com.crazymaker.cloud.center.config.ConfigServerApplication
 set JVM= -server -Xms1g -Xmx1g
 
 
-set APPLICATION_CONFIG= -Dserver.port=%SERVER_PORT%
+set APPLICATION_CONFIG=-Dserver.port=%SERVER_PORT% -Deureka.port=%EUREKA_PORT% -Deureka.host=%EUREKA_HOST%
 
 
 set DEBUG_OPTS=
